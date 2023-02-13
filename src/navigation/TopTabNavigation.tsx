cf5ff2f4236bc64cf4b4d56c640fad7fb9155ddd
sandeep singh <sandeepdhillon659@gmail.com>
2023-02-13 16:48:36 +0530

    react-native upgrade to 0.71.2

--- src/navigation/TopTabNavigation.tsx
+++ src/navigation/TopTabNavigation.tsx
@@ -13,28 +13,28 @@ import DrawerStack from './DrawerStack';
 type Props = NativeStackScreenProps<any>
 
 const TopTabNavigation: FC<Props> = () => {
-    const Tab = createMaterialTopTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
 
-    const { currentNavigationState: { data } } = useSelector((state => state)) as RootState
+  const { currentNavigationState: { data } } = useSelector((state => state)) as RootState
 
-    console.log("route++++++++++++++++", data)
+  console.log("route++++++++++++++++", data)
 
-    const enabledRoutes = [
-      ScreenConstants.FEED_SCREEN,
-      ScreenConstants.PROFILE_SCREEN,
+  const enabledRoutes = [
+    ScreenConstants.FEED_SCREEN,
+    ScreenConstants.PROFILE_SCREEN,
     //   ScreenConstants.HOME_SCREEN,
-      ScreenConstants.CHAT_SCREEN,
-    ];
-    return <Tab.Navigator screenOptions={{
-        swipeEnabled: (!data?.name || enabledRoutes.includes(data?.name)) ? true : false,
-    }}
-        tabBar={(Prosp) => null}>
-        <Tab.Screen
-            name={ScreenConstants.DRAWER_STACK}
-            component={DrawerStack}
-        />
-        <Tab.Screen name={ScreenConstants.PROFILE_SCREEN} component={ProfileScreen} />
-    </Tab.Navigator>
+    ScreenConstants.CHAT_SCREEN,
+  ];
+  return <Tab.Navigator screenOptions={{
+    swipeEnabled: (!data?.name || enabledRoutes.includes(data?.name)) ? true : false,
+  }}
+    tabBar={(Prosp) => null}>
+    <Tab.Screen
+      name={ScreenConstants.DRAWER_STACK}
+      component={DrawerStack}
+    />
+    <Tab.Screen name={ScreenConstants.PROFILE_SCREEN} component={ProfileScreen} />
+  </Tab.Navigator>
 }
 
 export default TopTabNavigation
